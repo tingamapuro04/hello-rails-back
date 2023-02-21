4ad5ecf5d532f5426580a96e213613296787d4bc
tingamapuro04 <alponcemobutu100@gmail.com>
2023-02-21 14:44:14 +0300

    Setup basic files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(language: "Danish", body: 'God morgen hvordan har du det?')
+Message.create(language: "Portugees", body: '¿Buenos días cómo estás?')
+Message.create(language: "German", body: 'Guten Morgen, wie geht es dir?')
+Message.create(language: "French", body: 'Bonjour comment-allez vous?')
+Message.create(language: "Dho Luo", body: 'Bom Dia como você está?')
+
